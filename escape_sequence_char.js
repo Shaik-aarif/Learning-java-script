96d74d7ff550345b7bbd5688c58f0b612dc36679
Shaik-aarif <0222learning@gmail.com>
2023-01-07 20:43:46 +0530

    up=down

--- escape_sequence_char.js
+++ escape_sequence_char.js
@@ -1,6 +1,9 @@
-// let str = 'Arif's Repo' // throws error
-let str1 ='Arif\'s Repo'
 // without  \
+// let str = 'Arif's Repo' // throws error
+
+
 // with \
+let str1 ='Arif\'s Repo'
+
 
 console.log(str1);
